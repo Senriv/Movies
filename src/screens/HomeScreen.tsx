029e571ce1e2db3a9737019e7556228f60e9ee44
Senriv <ikiry2013@gmail.com>
2024-12-23 16:58:33 +0200

    eas init

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,30 @@
-import { Text, View } from "react-native";
+import { Platform, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import { styles } from "../theme";
+
+const ios = Platform.OS == "ios";
 
 export default function HomeScreen() {
-  return <View className="flex-1 bg-neutral-800"></View>;
+  return (
+    <View className="flex-1 bg-neutral-800">
+      {/* search bar and logo */}
+      <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
+        <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+          <Text className="text-white text-3xl font-bold">
+            <Text style={styles.text}>M</Text>ovies
+          </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </View>
+  );
 }
